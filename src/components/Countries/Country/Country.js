a18029e61775872a51f93fcf0cfee40f0efd7e3f
itzmrnh <mdnh.42@gmail.com>
2023-04-17 00:54:46 +0600

    Rest Countries

---
+++ src/components/Countries/Country/Country.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Country.css';
+const Country = (props) => {
+    const {area, region, population, name, flags} = props.country;
+    return (
+        <div className='country'>
+            <img src={flags.png} alt="" />
+            <h1>Country Name:{name.common}</h1>
+            <h4>Population:{population}</h4>
+            <h4>Area:{area}</h4>
+            <p>Region: {region}</p>
+        </div>
+    );
+};
+
+export default Country;
